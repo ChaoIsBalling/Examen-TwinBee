af0221d0960f45d63d7c3e990fdb64e6f42491bd
Jiale <114423476+JialeHe28@users.noreply.github.com>
2023-12-26 19:15:35 +0100

    s

--- src/level.js
+++ src/level.js
@@ -1,68 +1,65 @@
 import Player from "./player.js";
-export default class Level extends Phaser.Scene
-{
-    constructor(){
-        super({key: "Level"});
+export default class Level extends Phaser.Scene {
+    constructor() {
+        super({ key: "Level" });
     }
-    init (data)
-    {
+    init(data) {
         console.log('init', data);
 
         this.multiplayer = data.multiplayer;
     }
-    create()
-    {   this.physics.world.drawDebug = false;
+    create() {
+        this.physics.world.drawDebug = false;
         this.toggleDebug = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.C);
 
-        this.bg=this.add.image(0, -1160, 'bg').setOrigin(0);
-        this.tweens.add(
-        {targets: this.bg,
-            y:+1,
-            ease: 'Linear',    
-            duration: 50000,
-            repeat: 0,           
-            yoyo: false,
+        this.bg = this.add.image(0, -1160, 'bg').setOrigin(0);
+        this.player = this.add.group({
+            maxSize: 2,
+            runChildUpdate: true
         });
-        var playerInput= this.input.keyboard.addKeys({
+        this.tweens.add(
+            {
+                targets: this.bg,
+                y: +1,
+                ease: 'Linear',
+                duration: 50000,
+                repeat: 0,
+                yoyo: false,
+            });
+        var playerInput = this.input.keyboard.addKeys({
             up: Phaser.Input.Keyboard.KeyCodes.W,
             down: Phaser.Input.Keyboard.KeyCodes.S,
             left: Phaser.Input.Keyboard.KeyCodes.A,
             right: Phaser.Input.Keyboard.KeyCodes.D,
             space: Phaser.Input.Keyboard.KeyCodes.SPACE,
-          });
-       if (this.multiplayer)
-       {
-        var player2Input= this.input.keyboard.addKeys({
-            up: Phaser.Input.Keyboard.KeyCodes.UP,
-            down: Phaser.Input.Keyboard.KeyCodes.DOWN,
-            left: Phaser.Input.Keyboard.KeyCodes.LEFT,
-            right: Phaser.Input.Keyboard.KeyCodes.RIGHT,
-            space: Phaser.Input.Keyboard.KeyCodes.ENTER,
-          });
-          var p2Pos=this.cameras.main.centerX+this.cameras.main.centerX/2;
-          this.player2=new Player(this,p2Pos,200,'player2',player2Input);
-          
-          this.player=new Player(this,this.cameras.main.centerX/2,200,'player',playerInput);
-       }
-       else
-       {
-        this.player=new Player(this,this.cameras.main.centerX,200,'player',playerInput);
-       }
+        });
+        if (this.multiplayer) {
+            var player2Input = this.input.keyboard.addKeys({
+                up: Phaser.Input.Keyboard.KeyCodes.UP,
+                down: Phaser.Input.Keyboard.KeyCodes.DOWN,
+                left: Phaser.Input.Keyboard.KeyCodes.LEFT,
+                right: Phaser.Input.Keyboard.KeyCodes.RIGHT,
+                space: Phaser.Input.Keyboard.KeyCodes.ENTER,
+            });
+            var p2Pos = this.cameras.main.centerX + this.cameras.main.centerX / 2;
+
+            this.player.add(new Player(this, p2Pos, 200, 'player2', player2Input));
+
+            this.player.add(new Player(this, this.cameras.main.centerX / 2, 200, 'player', playerInput));
+        }
+        else {
+            this.player.add(new Player(this, this.cameras.main.centerX, 200, 'player', playerInput));
+        }
     }
-    update()
-    { 
-        this.player.update();
-        if (this.multiplayer)
-        this.player2.update();
+    update() {
         if (Phaser.Input.Keyboard.JustDown(this.toggleDebug)) {
             if (this.physics.world.drawDebug) {
-              this.physics.world.drawDebug = false;
-              this.physics.world.debugGraphic.clear();
+                this.physics.world.drawDebug = false;
+                this.physics.world.debugGraphic.clear();
             }
             else {
-              this.physics.world.drawDebug = true;
+                this.physics.world.drawDebug = true;
             }
-        
+        }
     }
-}
 }
