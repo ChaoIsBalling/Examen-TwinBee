c2019d9ea44912501f74bd80ba719a4933571bff
Jiale <114423476+JialeHe28@users.noreply.github.com>
2023-12-26 12:24:47 +0100

    ddd

--- src/level.js
+++ src/level.js
@@ -3,4 +3,12 @@ export default class Level extends Phaser.Scene
     constructor(){
         super({key: "Level"});
     }
+    create()
+    {
+        this.load.image('bg');
+    }
+    update()
+    {
+        console.log("s");
+    }
 }
