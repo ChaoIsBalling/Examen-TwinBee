1139598927d1431694a06f166e94f96700f2ea6e
Jiale <114423476+JialeHe28@users.noreply.github.com>
2023-12-27 13:10:26 +0100

    e

---
+++ src/enemy.js
@@ -0,0 +1,5 @@
+export default class Enemy extends Phaser.GameObjects.Sprite{
+    constructor(scene,x,y, texture, cursors) {
+        super(scene,x,y,texture);
+    }
+}
---
+++ src/item.js
@@ -0,0 +1,5 @@
+export default class item extends Phaser.GameObjects.Sprite{
+    constructor(scene,x,y, texture, cursors) {
+        super(scene,x,y,texture);
+    }
+}
