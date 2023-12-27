a370d4640df46c44a7ed5871b906e3b483a3d9d2
Jiale <114423476+JialeHe28@users.noreply.github.com>
2023-12-27 19:06:43 +0100

    a fuck ton

--- src/level.js
+++ src/level.js
@@ -4,8 +4,11 @@ import item from "./item.js";
 export default class Level extends Phaser.Scene {
     constructor() {
         super({ key: "Level" });
-        this.enemyTime=1000;
-        this.itemTime=5000;
+        this.enemyTime = 1000;
+        this.itemTime = 5000;
+        this.transitionTime = 500;
+        this.levelEnded = false;
+        this.win = false;
     }
     init(data) {
         console.log('init', data);
@@ -17,17 +20,24 @@ export default class Level extends Phaser.Scene {
         this.toggleDebug = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.C);
 
         this.bg = this.add.image(0, -1160, 'bg').setOrigin(0);
-     
-        this.tweens.add(
+
+         var tween =this.tweens.add(
             {
                 targets: this.bg,
                 y: +1,
                 ease: 'Linear',
-                duration: 50000,
+                duration: 1000,
                 repeat: 0,
                 yoyo: false,
-            });   
-            
+            });
+
+            tween.on('complete', () => {
+               this.win = true;
+                this.levelFinish("WINNER :)")
+
+            })
+        
+
         this.bullet = this.add.group({
             maxSize: 100,
             runChildUpdate: true
@@ -45,12 +55,13 @@ export default class Level extends Phaser.Scene {
             runChildUpdate: true
         })
         this.playerInit();
-        this.enemy.add(new Enemy(this,226,20));
-        this.item.add(new item(this,this.cameras.main.centerX+60,20));
+        this.enemy.add(new Enemy(this, 100, -5));
+        this.physics.add.collider(this.player, this.enemy, this.touchEnemy, null, this);
     }
     update() {
         this.EnemySpawn();
         this.itemSpawn();
+        this.returnTitle();
         if (Phaser.Input.Keyboard.JustDown(this.toggleDebug)) {
             if (this.physics.world.drawDebug) {
                 this.physics.world.drawDebug = false;
@@ -61,9 +72,16 @@ export default class Level extends Phaser.Scene {
             }
         }
     }
-
-    playerInit()
-    {
+    touchEnemy(player, enemy) {
+        if(!this.win)
+        {
+        var death = this.sound.add('dead')
+        death.play();
+        player.destroy();
+        this.levelFinish("GAME OVER");
+        }
+    }
+    playerInit() {
         var playerInput = this.input.keyboard.addKeys({
             up: Phaser.Input.Keyboard.KeyCodes.W,
             down: Phaser.Input.Keyboard.KeyCodes.S,
@@ -89,22 +107,44 @@ export default class Level extends Phaser.Scene {
             this.player.add(new Player(this, this.cameras.main.centerX, 200, 'player', playerInput));
         }
     }
-    EnemySpawn()
-    {
-     this.enemyTime--;
-     if (this.enemyTime==0)
-     {var spawnPoint = Phaser.Math.Between(30, 226);
-        this.enemy.add(new Enemy(this,spawnPoint,0));
-        this.enemyTime =1000;
-     }
+    EnemySpawn() {
+        this.enemyTime--;
+        if (this.enemyTime <= 0) {
+            var spawnPoint = Phaser.Math.Between(30, 226);
+            this.enemy.add(new Enemy(this, spawnPoint, -5));
+            this.enemyTime = 1000;
+        }
     }
-    itemSpawn()
-    {
+    itemSpawn() {
         this.itemTime--;
-        if (this.itemTime==0)
-        {var spawnPoint = Phaser.Math.Between(30, 226);
-           this.item.add(new item(this,spawnPoint,0));
-           this.itemTime =5000;
+        if (this.itemTime <= 0) {
+            var spawnPoint = Phaser.Math.Between(30, 226);
+            this.item.add(new item(this, spawnPoint, 0));
+            this.itemTime = 5000;
+        }
+    }
+    levelFinish(smh) {
+        let text = this.add.text(this.cameras.main.centerX, 80, smh, {
+            fontFamily: 'gummy',
+        });
+        text.setOrigin(0.5, 0.5);
+        text.setAlign('center');
+        text.setFontSize(40);
+        var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        text.setStroke("#" + randomColor, 4)
+        text.setFill('#FFFFFF');
+        this.levelEnded = true;
+    }
+    returnTitle() {
+        if (this.levelEnded) {
+            this.transitionTime--;
+            if (this.transitionTime <= 0) {
+                this.levelEnded = false;
+                this.transitionTime = 500;
+                this.enemyTime = 1000;
+                this.itemTime = 5000;
+                this.scene.start('Title');
+            }
         }
     }
 }
